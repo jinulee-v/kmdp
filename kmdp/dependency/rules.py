467a425c1fea434cc27751a8dcd41666ca8a09c1
Jinu Lee <aquamrn@snu.ac.kr>
2021-05-01 20:26:42 +0900

    added adjective_SH rule

--- kmdp/dependency/rules.py
+++ kmdp/dependency/rules.py
@@ -121,9 +121,6 @@ class DefaultInterRule(KMDPRuleBase):
       if dep_wp[i]['pos_tag'] in label2head['all_heads']:
         # Intra-WP dependency
         return None
-    
-    if dep_wp == head_wp:
-      return None
 
     if kmdp_label not in dp_labels:
       raise KMDPRecoverException('default_inter', 'Invalid DP label: {}'.format(kmdp_label), dep_wp, dep_wp_i, head_wp, head_wp_i, kmdp_label)
@@ -160,4 +157,64 @@ class DefaultIntraRule(KMDPIntraWPRuleBase):
     # Not an intra-dependency
     return None
 
+
+@register_kmdp_rule("adjective_SH", ['default_inter', 'default_intra'])
+class AdjectiveSHRule(KMDPRuleBase):
+  """
+  Rules for adjective-like Chinese characters.
+  For default, Chinese characters(SH) are parsed as nouns.
+
+  - '故/SH' = 'dead'
+  - '新/SH' = 'new'
+  - '反/SH' = 'anti-'
+
+  """
+
+  def generate(cls, dep_wp, dep_wp_i, head_wp, dp_label):
+    dep_morph = dep_wp[dep_wp_i]
+    if dep_morph['text'] not in SH_adjectives:
+      # If not adjective-like chinese character, do nothing.
+      return None
     
+    # If head PoS, find next head morpheme.
+    for i in range(dep_wp_i+1, len(dep_wp)):
+      if dep_wp[i]['pos_tag'] in label2head['NP_heads']:
+        # Intra-WP dependency
+        return {
+          'dep': dep_morph['id'],
+          'head': dep_wp[i]['id'],
+          'label': 'DP'
+        }
+    
+    # if dep_wp_i-th morpheme is the last head of dep_wp:
+    # Inter-WP dependency
+    # Return the last head morpheme from the head_wp.
+    for i in reversed(range(len(head_wp))):
+      if head_wp[i]['pos_tag'] in label2head['all_heads']:
+        return {
+          'dep': dep_morph['id'],
+          'head': head_wp[i]['id'],
+          'label': 'DP' + ('_' + dp_label.split('_')[-1] if '_' in dp_label else '')
+        }
+    raise KMDPGenerateException('adjective_SH', 'Unexpected reach of end', dep_wp, dep_wp_i, head_wp, dp_label)
+  
+  def recover(cls, dep_wp, dep_wp_i, head_wp, head_wp_i, kmdp_label):
+    dep_morph = dep_wp[dep_wp_i]
+    if dep_morph['text'] not in SH_adjectives:
+      # If not adjective-like chinese character, do nothing.
+      return None
+    
+    # If head PoS, find next head morpheme.
+    for i in range(dep_wp_i+1, len(dep_wp)):
+      if dep_wp[i]['pos_tag'] in label2head['all_heads']:
+        # Intra-WP dependency
+        return None
+
+    if kmdp_label not in dp_labels:
+      raise KMDPRecoverException('default_inter', 'Invalid DP label: {}'.format(kmdp_label), dep_wp, dep_wp_i, head_wp, head_wp_i, kmdp_label)
+
+    return {
+      'dep': None,
+      'head': None,
+      'label': 'NP' # for 
+    }
